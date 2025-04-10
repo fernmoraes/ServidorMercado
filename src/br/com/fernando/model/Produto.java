f9898a5e3fb694cfb573675be185d69bcbefb0d1
Fernando Navajas Moraes <100091001+fernmoraes@users.noreply.github.com>
2025-04-10 11:00:43 -0300

    Seção Produto.java feito

--- src/br/com/fernando/model/Produto.java
+++ src/br/com/fernando/model/Produto.java
@@ -1,4 +1,34 @@
 package br.com.fernando.model;
 
+// Classe que representa um Produto do estoque
 public class Produto {
+
+    // Atributos privados (encapsulamento)
+    private String nome;
+    private int quantidade;
+    private String valorUnitario;
+
+    // Construtor com parâmetros
+    public Produto(String nome, int quantidade, String valorUnitario) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valorUnitario = valorUnitario;
+    }
+
+    // Getters para acessar os atributos
+    public String getNome() {
+        return nome;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public String getValorUnitario() {
+        return valorUnitario;
+    }
+
+    // Metodo toString sobrescrito para formatar como linha de CSV
+    @Override
+    public String toString() {
+        return nome + "; " + quantidade + "; " + valorUnitario + ";";
+    }
 }
